e8ede892a67a92439a23d423b0cb66fdb4ab6707
SergTsch87 <sergtsch87@gmail.com>
2024-11-24 11:06:38 +0200

    Initial commit. Adding file main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+# env1\bin\python -m pip freeze > requirements.txt
+# env2\bin\python -m pip install -r requirements.txt
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
